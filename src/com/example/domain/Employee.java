f5762b250c7d8849756b8ef6f604c67049174cfb
3ef1rus <10kmmrza10k@mail.ru>
2022-09-25 22:49:57 +0300

    create class

---
+++ src/com/example/domain/Employee.java
@@ -0,0 +1,9 @@
+package com.example.domain;
+
+public class Employee {
+    public int empId;
+    public String name;
+    public String ssn;
+    public double salary;
+    public Employee() {}
+}
